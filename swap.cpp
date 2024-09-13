3f8e2a95f6991b0a542b8dd876c995da064f2f30
Aradya Arvind Shetty <shettyaradya1234@gmail.com>
2024-09-13 22:33:56 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+int xmax, ymax;
+int xmid, ymid;
+int xmin, ymin;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
@@ -10,6 +12,15 @@ int main() {
     if (a > b) swap(a, b);
     cout << a << " " << b << " " << c << endl;
 
+    xmax = max({arr[0][0],arr[1][0],arr[2][0]});
+    ymax = max({arr[0][1],arr[1][1],arr[2][1]});
+        
+    xmin = min({arr[0][0],arr[1][0],arr[2][0]});
+    ymin = min({arr[0][1],arr[1][1],arr[2][1]});
+        
+    xmid = arr[0][0] + arr[1][0] + arr[2][0] - xmin - xmax;
+    ymid = arr[0][1] + arr[1][1] + arr[2][1] - ymin - ymax;
+        
     sort(arr, arr + 3, [](const int a[2], const int b[2]) {
         return a[0] < b[0];
     });
